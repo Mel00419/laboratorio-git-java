52a08e0d879b4fcaebb129f368abde3970a3ffe1
Genesis Melissa Cruz Vega <202301414@ujmd.edu.sv>
2026-09-07 03:07:31 -0600

    Agregar cálculo del promedio

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Ingrese nombre: ");
-        String nombre = teclado.nextLine();
-        System.out.print("Ingrese edad: ");
-        int edad = teclado.nextInt();
+        System.out.println("CÁLCULO DE PROMEDIO");
+        System.out.print("Ingrese nota 1: ");
+        double nota1 = teclado.nextDouble();
+        System.out.print("Ingrese nota 2: ");
+        double nota2 = teclado.nextDouble();
+        System.out.print("Ingrese nota 3: ");
+        double nota3 = teclado.nextDouble();
 
-        Estudiante estudiante = new Estudiante(nombre, edad);
-        System.out.println();
-        System.out.println("ESTUDIANTE REGISTRADO");
-        estudiante.mostrarInformacion();
+        double promedio = (nota1 + nota2 + nota3) / 3;
+        System.out.println("Promedio: " + promedio);
     }
 }
 
