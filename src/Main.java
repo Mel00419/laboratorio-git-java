5afea8b87e205bd4e9c1f83fc796c4115b1ab3d1
Genesis Melissa Cruz Vega <202301414@ujmd.edu.sv>
2026-09-07 02:45:02 -0600

    Crear proyecto inicial en Java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("====================================");
+        System.out.println(" LABORATORIO DE DESARROLLO SOFTWARE ");
+        System.out.println("====================================");
+        System.out.println("Mi primer proyecto utilizando Java y Git");
+    }
+}
