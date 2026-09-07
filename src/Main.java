1e3b7b149a0809c81cfb7448d2fd9ab5b7b51aa2
Genesis Melissa Cruz Vega <202301414@ujmd.edu.sv>
2026-09-07 03:37:10 -0600

    Agregar validación de notas

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,15 @@ public class Main {
         System.out.print("Ingrese nota 3: ");
         double nota3 = teclado.nextDouble();
 
-        double promedio = (nota1 + nota2 + nota3) / 3;
-        System.out.println("Promedio: " + promedio);
+        if (nota1 < 0 || nota1 > 10 ||
+                nota2 < 0 || nota2 > 10 ||
+                nota3 < 0 || nota3 > 10) {
+            System.out.println("Error: las notas deben estar entre 0 y 10");
+        } else {
+            double promedio = (nota1 + nota2 + nota3) / 3;
+            System.out.println("Promedio: " + promedio);
+        }
     }
 }
+
 
