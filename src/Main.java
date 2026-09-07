5435c920aadd6ba00629b66dba763d83c84947a7
Genesis Melissa Cruz Vega <202301414@ujmd.edu.sv>
2026-09-07 03:49:53 -0600

    Agregar estado aprobado y reprobrado

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("CÁLCULO DE PROMEDIO");
+        System.out.println("CÁLCULO DE PROMEDIO Y ESTADO");
         System.out.print("Ingrese nota 1: ");
         double nota1 = teclado.nextDouble();
         System.out.print("Ingrese nota 2: ");
@@ -18,8 +18,16 @@ public class Main {
         } else {
             double promedio = (nota1 + nota2 + nota3) / 3;
             System.out.println("Promedio: " + promedio);
+
+            if (promedio >= 6) {
+                System.out.println("Estado: APROBADO");
+            } else {
+                System.out.println("Estado: REPROBADO");
+            }
         }
     }
 }
+
+
 
 
